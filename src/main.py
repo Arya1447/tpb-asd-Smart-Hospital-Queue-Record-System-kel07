1cfeead9a4707ff44fb2641e19b16a790d722dda
Arya1447 <sadewoarya94@gmail.com>
2026-05-16 14:18:19 +0700

    update code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print ("ditunggu nggeh pak")
+def main():
+    queues = {poli: PriorityQueue() for poli in POLI}
+    dokter_stacks = {i: Stack() for i in range(len(POLI))}
+    bst_rm = BSTRekamMedis()
+    counter = 0
+
+    # TODO: implementasikan loop CLI
+    print('Smart Hospital Queue System Ketik BANTUAN untuk daftar perintah')
+
+if __name__ == '__main__':
+    main()
