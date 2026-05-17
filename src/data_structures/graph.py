504032fcfc96b62c4e63e0a0baa22c08218ae840
Arya1447 <sadewoarya94@gmail.com>
2026-05-17 11:09:26 +0700

    uodate code by arya

--- src/data_structures/graph.py
+++ src/data_structures/graph.py
@@ -1,1 +1,16 @@
-print("sebentar nggih pak")
+class Graph:
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+
+        if u not in self.graph:
+            self.graph[u] = []
+
+        self.graph[u].append(v)
+
+    def display(self):
+
+        for node in self.graph:
+            print(node, '->', self.graph[node])
