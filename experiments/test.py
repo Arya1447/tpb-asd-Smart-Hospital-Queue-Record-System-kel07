a837ab24139b0be48a91383b4257126d272fe910
Arya1447 <sadewoarya94@gmail.com>
2026-05-20 18:12:27 +0700

    uji kecepatan bst dan priority queue

--- experiments/test.py
+++ experiments/test.py
@@ -1,1 +1,114 @@
-print('hello')
+import sys
+import os
+
+sys.path.append(
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..'
+        )
+    )
+)
+
+# jika ingin menjalankan ini harap ubah priotity que ue bagian 'from data_structures.node import Node' manjadi 'src.from data_structures.node import Node'
+
+import numpy as np
+import time
+import random
+
+from src.data_structures.priority_queue import PriorityQueue
+from src.data_structures.bst import BSTRekamMedis
+
+from src.models.pasien import Pasien
+from src.models.rekam_medis import RekamMedis
+
+np.random.seed(42)
+random.seed(42)
+
+
+def test_priority_queue(n):
+
+    queue = PriorityQueue()
+
+    start = time.time()
+
+    for i in range(n):
+
+        pasien = Pasien(
+            i,
+            f'Pasien{i}',
+            'Umum',
+            random.randint(1, 3),
+            time.time()
+        )
+
+        queue.enqueue(pasien)
+
+    end = time.time()
+
+    print(
+        f'Enqueue {n} pasien '
+        f': {end - start:.5f} detik'
+    )
+
+    start = time.time()
+
+    while not queue.is_empty():
+
+        queue.dequeue()
+
+    end = time.time()
+
+    print(
+        f'Dequeue {n} pasien '
+        f': {end - start:.5f} detik'
+    )
+
+
+def test_bst(n):
+
+    bst = BSTRekamMedis()
+
+    start = time.time()
+
+    for i in range(n):
+
+        rm = RekamMedis(
+            i,
+            f'Pasien{i}',
+            'Umum',
+            []
+        )
+
+        bst.insert(rm)
+
+    end = time.time()
+
+    print(
+        f'Insert BST {n} data '
+        f': {end - start:.5f} detik'
+    )
+
+    start = time.time()
+
+    bst.search(n // 2)
+
+    end = time.time()
+
+    print(
+        f'Search BST {n} data '
+        f': {end - start:.8f} detik'
+    )
+
+
+print('\n=== EXPERIMENT PRIORITY QUEUE ===')
+
+for n in [50, 200, 500, 1000]:
+
+    test_priority_queue(n)
+
+print('\n=== EXPERIMENT BST ===')
+
+for n in [50, 200, 500, 1000]:
+
+    test_bst(n)
---
+++ tests/test_bst.py
@@ -0,0 +1,61 @@
+class BSTNode:
+
+    def __init__(self, rekord):
+        self.rekord = rekord
+        self.left = None
+        self.right = None
+
+
+class BSTRekamMedis:
+
+    def __init__(self):
+        self.root = None
+
+    def insert(self, rekord):
+
+        new_node = BSTNode(rekord)
+
+        # jika root kosong
+        if self.root is None:
+            self.root = new_node
+            return
+
+        current = self.root
+
+        while True:
+
+            # ke kiri
+            if rekord.no_rm < current.rekord.no_rm:
+
+                if current.left is None:
+                    current.left = new_node
+                    return
+
+                current = current.left
+
+            # ke kanan
+            else:
+
+                if current.right is None:
+                    current.right = new_node
+                    return
+
+                current = current.right
+
+    def search(self, no_rm):
+
+        current = self.root
+
+        while current:
+
+            if no_rm == current.rekord.no_rm:
+                return current.rekord
+
+            elif no_rm < current.rekord.no_rm:
+                current = current.left
+
+            else:
+                current = current.right
+
+        return None
+    
